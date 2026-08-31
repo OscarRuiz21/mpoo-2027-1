8c2c5adcd72a0af3a3a39a83059e289b9e7dc25d
OscarRu <oscar.ruizhu@gmail.com>
2026-08-31 15:32:51 -0600

    P01: prueba demo 2

--- entregas/ruiz_oscar/p01/HolaMundo.java
+++ entregas/ruiz_oscar/p01/HolaMundo.java
@@ -2,6 +2,6 @@ package p01;
 
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("¡Hola, Mundo! - Oscar Ruiz ");
+        System.out.println("¡Hola, Mundo! - Oscar Ruiz, otro cambio!");
     }
 }
