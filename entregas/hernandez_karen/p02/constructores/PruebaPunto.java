96ead12c4bcc90f0cd90857a4b688d27fe393456
Karen Hernández <karenhernandezav.fi@gmail.com>
2026-09-02 16:32:16 -0600

    Agrega carpeta p02

---
+++ entregas/hernandez_karen/p02/constructores/PruebaPunto.java
@@ -0,0 +1,13 @@
+public class PruebaPunto {
+    public static void  main(String[] args){
+        Punto p=new Punto();
+        p.x=5;
+        p.y=8;
+        p.imprimePunto();
+
+        Punto x=new Punto();
+        x.x=7;
+        x.y=2;//en la guia esta x.x=2,pero seria modificar la misma coordenada x
+        x.imprimePunto();
+    }  
+}
