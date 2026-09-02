6d0351ac58b53838a688eec27314aaba9486e515
Karen Hernández <karenhernandezav.fi@gmail.com>
2026-09-02 16:51:09 -0600

    Instancias que reciban valores

--- entregas/hernandez_karen/p02/constructores/PruebaPunto.java
+++ entregas/hernandez_karen/p02/constructores/PruebaPunto.java
@@ -1,13 +1,9 @@
-public class PruebaPunto {
-    public static void  main(String[] args){
-        Punto p=new Punto();
-        p.x=5;
-        p.y=8;
+public class PruebaPunto{
+    public static void  main(String[] args) {
+        Punto p=new Punto(5,8);
         p.imprimePunto();
 
-        Punto x=new Punto();
-        x.x=7;
-        x.y=2;//en la guia esta x.x=2,pero seria modificar la misma coordenada x
+        Punto x=new Punto(7,2);
         x.imprimePunto();
     }  
 }
