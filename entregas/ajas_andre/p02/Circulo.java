a02828e924e42eb702f45171f0b7819344214a1e
andre Ajas <andre.ajas.ruiz@gmail.com>
2026-09-02 13:55:40 -0600

    P02: Creación de la clase Circulo

---
+++ entregas/ajas_andre/p02/Circulo.java
@@ -0,0 +1,9 @@
+public class Circulo {
+    public static void main(String[] args) {
+        System.out.println("¡Hola, Desde Círculo! - André Ajas, otro cambio!");
+    }
+
+    public static double area(double radio) {
+        return Math.PI * radio * radio;
+    }
+}
