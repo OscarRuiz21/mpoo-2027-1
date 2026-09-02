c23ef8a62bde0fbabc5fdbcafe1e5bcb69ee4f93
Emilio Hernandez <emihc2310@gmail.com>
2026-09-02 16:03:08 -0600

    P02: Agrega archivos Punto.java y PruebaPunto.java

---
+++ entregas/Emilio_Hernandez/p03/Punto.java
@@ -0,0 +1,17 @@
+public class Punto {
+    int x, y;
+
+    // Constructor por defecto
+    public Punto() {
+    }
+
+    // Constructor con parámetros
+    public Punto(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void imprimePunto() {
+        System.out.println("Punto [x=" + x + ", y=" + y + "]");
+    }
+}
