277dfef015ba77845bc61fd29cdb79c6758b5236
Leon Carrasco Jonatan Abdel <jonca2829@gmail.com>
2026-09-03 14:39:05 -0600

    Ejemplos P02

---
+++ entregas/leon_jonatan/p02/Ejemplo2.java
@@ -0,0 +1,16 @@
+/*
+public class Ejemplo2 {
+
+    public static void main(String[] args) {
+        Punto p = new Punto();
+        p.x = 5;
+        p.y = 8;
+        p.imprimePunto();
+
+        Punto x = new Punto();
+        x.x = 7;
+        x.x = 2; // Nota: En la última asignación de la imagen dice x.x = 2 (sobrescribe la x). Si debía ser la y, cambiar a x.y = 2.
+        x.imprimePunto();
+    }
+}
+    */
