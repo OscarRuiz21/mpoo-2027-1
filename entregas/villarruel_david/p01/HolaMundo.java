f93b218ac27b1a02a326f745f5bd996b80a89e88
David Uriel Villarruel Sanchez <villarrueldavid43@gmail.com>
2026-09-05 22:34:09 -0600

    P01: Hola Mundo

---
+++ entregas/villarruel_david/p01/HolaMundo.java
@@ -0,0 +1,8 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        String nombre = "Uriel";
+        String apellido = "Villarruel";
+
+        System.out.println("Hola, soy " + nombre + " " + apellido);
+    }
+}
