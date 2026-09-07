cb594e1d60e829bcfec3670668845173dc8efe1a
PauloCBravo <paulocbravo56@gmail.com>
2026-09-07 14:05:58 -0600

    Add files via upload

---
+++ entregas/bravo_paulo/p02/Puntoo.java
@@ -0,0 +1,13 @@
+public class Puntoo {
+    int x,y;
+    public Puntoo (int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void imprimePuntoo() {
+        System.out.println("Punto [x=" + x + ", y=" + y + "]");
+
+    }
+
+}
