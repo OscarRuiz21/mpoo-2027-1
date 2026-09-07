feb625c094d47cbe5a40729ceec890b575f51bea
CSAngel26 <castilloserratoangel5@gmail.com>
2026-09-06 18:27:02 -0600

    Eliminar HolaMundo.java

--- entregas/angel_castillo/HolaMundo.java
+++
@@ -1,7 +0,0 @@
-public class HolaMundo {
-    public static void main(String[] args) {
-        String nombre = "Angel";
-        String apellido = "Castillo"; 
-        System.out.println("Hola, soy " + nombre + " " + apellido);
-    }
-}
