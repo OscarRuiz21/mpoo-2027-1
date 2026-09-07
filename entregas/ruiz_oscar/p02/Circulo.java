bf363573bcaf416b7ba8d7c784a60bfdcfc86c62
Oscar Ruiz <oscar.ruizhu@gmail.com>
2026-09-07 12:52:25 -0600

    Rama de ejemplo del profesor: entregas/ruiz_oscar con P00-P02 de demo

---
+++ entregas/ruiz_oscar/p02/Circulo.java
@@ -0,0 +1,11 @@
+package p02;
+
+public class Circulo {
+    public static void main(String[] args) {
+        System.out.println("¡Hola, Desde Circulo! - Oscar Ruiz, otro cambio!");
+    }
+
+    public static double area(double radio) {
+        return Math.PI * radio * radio;
+    }
+}
