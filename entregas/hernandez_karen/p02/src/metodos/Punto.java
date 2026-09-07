a9e82fd140399d3403289cc56f7f4bcadb224bc1
Karen Hernández <karenhernandezav.fi@gmail.com>
2026-09-07 15:06:52 -0600

    Correccion p02

--- entregas/hernandez_karen/p02/src/metodos/Punto.java
+++ entregas/hernandez_karen/p02/src/metodos/Punto.java
@@ -1,11 +1,5 @@
-public class Punto {
+public class Punto{
     int x,y;
-
-    public Punto(){
-        this.x=x; 
-        this.y=y;
-    }
-
     public void imprimePunto(){
         System.out.println("Punto [x=" +x+ ",y="+ y +"]");
     }
