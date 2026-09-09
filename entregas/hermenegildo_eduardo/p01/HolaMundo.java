b1ef2fd517d9c7493a811dc8d044b27aff931962
eduhetmefi <eduhetmefi@gmail.com>
2026-09-09 12:17:23 -0600

    P01:entorno y hola mundo con mi nombre

---
+++ entregas/hermenegildo_eduardo/p01/HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        String nombre = "Eduardo";      
+        String apellido = "Hermenegildo"; 
+        System.out.println("Hola, soy " + nombre + " " + apellido);
+    }
+}
