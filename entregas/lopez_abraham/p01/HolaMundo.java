ca525ac1f3538594b7b287e3a98ebc2564012d73
Abraham_Lopez <muzzyab20@gmail.com>
2026-09-14 02:38:19 -0600

    Add files via upload

---
+++ entregas/lopez_abraham/p01/HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        String nombre = "Abraham";      // el tuyo
+        String apellido = "Lopez"; // el tuyo
+        System.out.println("Hola, soy " + nombre + " " + apellido);
+    }
+}
