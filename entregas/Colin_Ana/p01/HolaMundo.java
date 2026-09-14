bd9c5edf88f169126cf256c49710ec3a1ac4d651
Ana Ivonne Colin Torres <ana.colin.torres@gmail.com>
2026-09-13 18:07:10 -0600

    Agregando carpeta

--- entregas/Colin_Ana/p01/HolaMundo.java
+++
@@ -1,7 +0,0 @@
-public class HolaMundo {
-    public static void main(String[] args) {
-        String nombre = "Ana Ivonne";     
-        String apellido = "Colin Torres"; 
-        System.out.println("Hola, soy Ana Ivonne Colin Torres");
-    }
-}
