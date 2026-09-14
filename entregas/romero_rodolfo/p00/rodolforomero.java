c39f2e18d002c95732090723fe772e4558377f7e
Rodolfo_Aragom_Romero_Sandoval <rodolfoaragom321@gmail.com>
2026-09-13 19:37:24 -0600

    Agregar P00 y actualizar P01 con nombre

---
+++ entregas/romero_rodolfo/p00/rodolforomero.java
@@ -0,0 +1,8 @@
+public class rodolforomero {
+    public static void main(String[] args) {
+        String nombre = "Rodolfo";     
+        String apellido = "Romero"; 
+        System.out.println("Hola, soy " + nombre + " " + apellido);
+        System.out.println("Esta es una prueba de Git");
+    }
+}
